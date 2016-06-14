d0c30f70d021d21f4c1496c4d5db719fdf725464
Chris Hutchinson <chris@cshutchinson.com>
2016-06-14 19:35:28 -0400

    added missing discord.py dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setuptools.setup(
     license='BSD',
     packages=setuptools.find_packages(),
     install_requires=[
-        'PyYAML>=3.11'
+        'PyYAML>=3.11',
+        'discord.py>=0.9.2'
     ],
     scripts=[
         'bin/lab2635_soundboard.py'
